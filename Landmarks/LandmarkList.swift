262b266a679a832451cb08abcd9359b0ad702a51
Makan <makanfofana246@gmail.com>
2020-11-01 15:56:20 -0500

    Handling user input section completion

--- Landmarks/LandmarkList.swift
+++ Landmarks/LandmarkList.swift
@@ -8,11 +8,22 @@ A view showing a list of landmarks.
 import SwiftUI
 
 struct LandmarkList: View {
+//    @State var showFavoritesOnly = false
+    @EnvironmentObject var userData: UserData
+    
     var body: some View {
         NavigationView {
-            List(landmarkData) { landmark in
-                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $userData.showFavoritesOnly) {
+                    Text("Favorites Only")
+                }
+                
+                ForEach(userData.landmarks) { landmark in
+                    if !self.userData.showFavoritesOnly || landmark.isFavorite {
+                        NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                            LandmarkRow(landmark: landmark)
+                        }
+                    }
                 }
             }
             .navigationBarTitle(Text("Landmarks"))
@@ -22,11 +33,14 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone 12 mini", "iPhone XS Max", "Iphone 8"], id: \.self) {
-            deviceName in
-            LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)
-        }
+//        ForEach(["iPhone 12 mini", "iPhone XS Max", "Iphone 8"], id: \.self) {
+//            deviceName in
+//            LandmarkList()
+//                .previewDevice(PreviewDevice(rawValue: deviceName))
+//                .previewDisplayName(deviceName)
+//                .environmentObject(UserData())
+//        }
+        LandmarkList()
+            .environmentObject(UserData())
     }
 }
